f42fa90fc0c610db40d26957ad6970fa9559065d
soum-sr <soum1071@gmail.com>
2020-06-02 00:39:54 +0530

    added hosting files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit runn app.py
--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st 
 import os
-import numpy as np 
 import pandas as pd
 import seaborn as sns 
 import matplotlib.pyplot as plt
