f3ea92a3bf9d3e02c107ac135d21ce540df91e01
soum-sr <soum1071@gmail.com>
2020-06-01 22:11:49 +0530

    upgraded

--- app.py
+++ app.py
@@ -11,17 +11,27 @@ def select_dataset_file():
 	selected_dataset = st.selectbox("",filenames)
 	return os.path.join(dataset_dir, selected_dataset)
 
+def check_dataset_category(filename):
+	f_name = filename[11:]
+	regression = ['BostonHousing.csv']
+	sequence = ['livestock.csv','guinearice.csv']
+	classification = ['wine.csv','iris.csv', 'Zoo.csv','abalone.csv','BreastCancer.csv']
+	if f_name in regression:
+		return 'Regression'
+	if f_name in sequence:
+		return 'Sequence'
+	if f_name in classification:
+		return 'Classification'
 
 def main():
 	# Title 
 	st.title('Explore Standard Machine Learning Datasets')
-
 	# Dropdown option to select the dataset
 	st.write("### Select a Dataset from below: ")
 	selected_dataset = select_dataset_file()
-	st.info(f'Selected {selected_dataset}')
+	dataset_type = check_dataset_category(selected_dataset)
+	st.info(f'Dataset Type: {dataset_type}')
 
-	# option to view the head(parameter input) of dataframe
 	# Read the csvfile
 	df = pd.read_csv(selected_dataset)
 
@@ -52,21 +62,23 @@ def main():
 		st.write(df.describe())
 
 	# Value count of target values
-	if st.checkbox("Show Value Counts and Pie Chart of Target Columns"):
-		st.write(df.iloc[:,-1].value_counts())
-		st.write(df.iloc[:,-1].value_counts().plot.pie(autopct="%1.1f%%"))
-		st.pyplot()
+	if dataset_type == 'Classification':
+		if st.checkbox("Show Value Counts and Pie Chart of Target Columns"):
+			st.write(df.iloc[:,-1].value_counts())
+			st.write(df.iloc[:,-1].value_counts().plot.pie(autopct="%1.1f%%"))
+			st.pyplot()
 
 	# Coorelation plot of dataset
-	if st.checkbox("Show Coorelation Plot"):
-		st.write("### Heatmap")
-		fig, ax = plt.subplots(figsize=(10,10))
-		st.write(sns.heatmap(df.corr(), annot=True,linewidths=0.5))
-		st.pyplot()
+	if dataset_type == 'Regression' or dataset_type == 'Classification':
+		if st.checkbox("Show Coorelation Plot"):
+			st.write("### Heatmap")
+			fig, ax = plt.subplots(figsize=(10,10))
+			st.write(sns.heatmap(df.corr(), annot=True,linewidths=0.5))
+			st.pyplot()
 
 	# Select column(s)=>>multiple like tags to plot
-	
-	
+
+
 	# Select plotting style: area, bar, line, hist, box
 
 if __name__ == '__main__':
